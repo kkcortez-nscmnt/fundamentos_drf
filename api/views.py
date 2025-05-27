5d61268d87d8b04d7b577084674e45ffa3db6bf3
Jamilson do Nascimento <jamil.pyhidrodev@gmail.com>
2025-05-27 16:18:24 -0300

    chore: comentario informativo

--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 
 def students_view(request):
 
-    # primeiro exemplo respostau em formato json
+    # primeiro exemplo resposta em formato json
     students = {
         'id': 1,
         'name': 'Rathan',
