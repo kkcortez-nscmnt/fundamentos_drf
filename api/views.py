a8157e412007bfe730f7c7da228cd7135ebda654
Jamilson do Nascimento <jamil.pyhidrodev@gmail.com>
2025-05-27 18:17:01 -0300

    chore: implementa function view para GET e POST

--- api/views.py
+++ api/views.py
@@ -7,10 +7,19 @@ from students.models import Student
 from .serializers import StudentSerializer
 
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def students_view(request):
     if request.method == "GET":
-        # recuperando todos os dados
+        # recuperando todos recursos registrados em tabela no banco
         students = Student.objects.all()
         serializer = StudentSerializer(students, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == "POST":
+        # criando registro de recurso em tabela no banco
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        print(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
