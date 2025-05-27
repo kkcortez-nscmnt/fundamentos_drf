2b27cb005ac769639b50e31b5cc587ceae171d12
Jamilson do Nascimento <jamil.pyhidrodev@gmail.com>
2025-05-27 16:53:24 -0300

    style: configuta pre-commit

--- api/views.py
+++ api/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
 from django.http import JsonResponse
+from django.shortcuts import render
+
 from students.models import Student
 
 
 def students_view(request):
 
     # primeiro exemplo resposta em formato json
-    students =Student.objects.all() # QuerySet nao é seriaizavel.
-    students_list = list(students.values()) # Serialização Manual
+    students = Student.objects.all()  # QuerySet nao é seriaizavel.
+    students_list = list(students.values())  # Serialização Manual
     return JsonResponse(students_list, safe=False)
