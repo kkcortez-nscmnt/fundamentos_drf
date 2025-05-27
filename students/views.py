6de7b81abfcdc2b8c290acf7a06a0a76119a1e79
Jamilson do Nascimento <jamil.pyhidrodev@gmail.com>
2025-05-27 11:24:23 -0300

    chore: primeiro exemplo insercao de dados na view utilizando uma lista.

--- students/views.py
+++ students/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def students(request):
-    return HttpResponse("<h2>Hello World !!!</h2>")
+    # primeiro exemplo insercao de dados na view utilizando lista.
+    students = [
+        {"id": 1, "name": "John Doe", "age": 25}
+    ]
+    return HttpResponse(students)
