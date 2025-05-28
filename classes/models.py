9e7a429091b252c18660132f3fb3d7d055c5ff1e
Jamilson do Nascimento <jamil.pyhidrodev@gmail.com>
2025-05-28 16:41:15 -0300

    chore: implementa modelo class

--- classes/models.py
+++ classes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Class(models.Model):
+    cls_id = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    field = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
