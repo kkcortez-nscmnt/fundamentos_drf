71a367a8b518c9c8721c016a25fce3f5ec31c41d
Jamilson do Nascimento <jamil.pyhidrodev@gmail.com>
2025-05-28 10:34:08 -0300

    chore: implementa put em EmployeeDetailView

--- api/views.py
+++ api/views.py
@@ -80,3 +80,12 @@ class EmployeeDetailView(APIView):
         employee = self.get_object(pk)
         serializer = EmployeeSerializer(employee)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        employee = self.get_object(pk)
+        serializer = EmployeeSerializer(employee, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        print(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
